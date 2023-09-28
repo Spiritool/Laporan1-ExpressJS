6625c6766d0008742fa389849ba85a9bcb580cc6
Spiritool <mohammadshobri45@gmail.com>
2023-09-28 11:20:32 +0700

    dsassds

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Halo decks')
-})
+// app.get('/', (req, res) => {
+//     res.send('Halo decks')
+// })
+
+const bodyPs = require('body-parser');
+app.use(bodyPs.urlencoded({ extended: false}));
+app.use(bodyPs.json());
+
+const mhsRouter = require('./routes/mahasiswa.js');
+app.use('/api/mhs', mhsRouter);
 
 app.listen(port, () => {
     console.log(`aplikasi berjalan di http:://localhost:${port}`)
